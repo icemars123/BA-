7f87f53a07d86e154aaa76d4fd0efb7b35e997d6
icemars123 <gavinniubi@gmail.com>
2018-09-12 21:29:13 +1000

    commit

--- routes/tab-permission-templates.js
+++ routes/tab-permission-templates.js
@@ -1,6 +1,6 @@
 var permissiontemplatesTabWidgetsLoaded = false;
 
-console.log('gavin');
+
 
 function doPermissionTemplatesTabSearch(value, name) 
 {
@@ -16,21 +16,190 @@ function doPermissionTemplatesTabWidgets()
 
     permissiontemplatesTabWidgetsLoaded = true;
 
-    function doNewRoot() 
-    {
-        doServerDataMessage('newproducttemplate', { parentid: null, name: 'New Template', code: 'New Code' }, { type: 'refresh' });
-    }
+    // function doNewRoot() 
+    // {
+    //     doServerDataMessage('newproducttemplate', { parentid: null, name: 'New Template', code: 'New Code' }, { type: 'refresh' });
+    // }
 
     function doNew() 
     {
-        doTreeGridGetSelectedRowData
-            (
-            'divPermissionTemplatesTG',
-            function (row) 
+        console.log('gavin1');
+        var tb =
+            [
+                {
+                    text: 'Save',
+                    iconCls: 'icon-save',
+                    handler: doSave
+                }
+            ];
+
+
+
+        function doSave() {
+            var rows = $('#divPermissionTemplatedsPG').propertygrid('getRows');
+            var permissions =
             {
-                doServerDataMessage('newproducttemplate', { parentid: row.id, name: 'New Template' }, { type: 'refresh' });
+                canvieworders: rows[0].value,
+                cancreateorders: rows[1].value,
+                canviewinvoices: rows[2].value,
+                cancreateinvoices: rows[3].value,
+                canviewinventory: rows[4].value,
+                cancreateinventory: rows[5].value,
+                canviewpayroll: rows[6].value,
+                cancreatepayroll: rows[7].value,
+                canviewproducts: rows[8].value,
+                cancreateproducts: rows[9].value,
+                canviewclients: rows[10].value,
+                cancreateclients: rows[11].value,
+                canviewcodes: rows[12].value,
+                cancreatecodes: rows[13].value,
+                canviewusers: rows[14].value,
+                cancreateusers: rows[15].value,
+                canviewbuilds: rows[16].value,
+                cancreatebuilds: rows[17].value,
+                canviewtemplates: rows[18].value,
+                cancreatetemplates: rows[19].value,
+                canviewbanking: rows[20].value,
+                cancreatebanking: rows[21].value,
+                canviewpurchasing: rows[22].value,
+                cancreatepurchasing: rows[23].value,
+                canviewalerts: rows[24].value,
+                cancreatealerts: rows[25].value,
+                canviewdashboard: rows[26].value,
+                cancreatedashboard: rows[27].value
+            };
+
+            doServerDataMessage('saveuserpermissions', { useruuid: user.uuid, permissions: permissions }, { type: 'refresh' });
+        }
+
+
+
+        function doMakeRowProperty(name, value, group) {
+            var row =
+            {
+                name: name,
+                value: value,
+                group: group,
+                editor:
+                {
+                    type: 'checkbox',
+                    options:
+                    {
+                        on: 1,
+                        off: 0
+                    }
+                }
+            };
+
+            return row;
+        }
+
+        function doSaved(ev, args) {
+            $('#dlgUserPermissions').dialog('close');
+        }
+
+        $('#divEvents').on('saveuserpermissions', doSaved);
+
+        $('#dlgUserPermissions').dialog
+            (
+            {
+                title: 'Create Permission Template',
+                onClose: function () {
+                    $('#divEvents').off('saveuserpermissions', doSaved);
+                },
+                onOpen: function () {
+                    $('#divPermissionTemplatedsPG').propertygrid
+                        (
+                        {
+                            showGroup: true,
+                            scrollbarSize: 0,
+                            toolbar: tb,
+                            loader: function (param, success, error) {
+                                cache_userpermissions = [];
+
+                                cache_userpermissions.push(doMakeRowProperty('Can View', false, 'Orders'));
+                                cache_userpermissions.push(doMakeRowProperty('Can Create', false, 'Orders'));
+
+                                cache_userpermissions.push(doMakeRowProperty('Can View', false, 'Invoices'));
+                                cache_userpermissions.push(doMakeRowProperty('Can Create', false, 'Invoices'));
+
+                                cache_userpermissions.push(doMakeRowProperty('Can View', false, 'Inventory'));
+                                cache_userpermissions.push(doMakeRowProperty('Can Create', false, 'Inventory'));
+
+                                cache_userpermissions.push(doMakeRowProperty('Can View', false, 'Payroll'));
+                                cache_userpermissions.push(doMakeRowProperty('Can Create', false, 'Payroll'));
+
+                                cache_userpermissions.push(doMakeRowProperty('Can View', false, 'Products'));
+                                cache_userpermissions.push(doMakeRowProperty('Can Create', false, 'Products'));
+
+                                cache_userpermissions.push(doMakeRowProperty('Can View', false, 'Clients'));
+                                cache_userpermissions.push(doMakeRowProperty('Can Create', false, 'Clients'));
+
+                                cache_userpermissions.push(doMakeRowProperty('Can View', false, 'Codes'));
+                                cache_userpermissions.push(doMakeRowProperty('Can Create', false, 'Codes'));
+
+                                cache_userpermissions.push(doMakeRowProperty('Can View', false, 'Users'));
+                                cache_userpermissions.push(doMakeRowProperty('Can Create', false, 'Users'));
+
+                                cache_userpermissions.push(doMakeRowProperty('Can View', false, 'Builds'));
+                                cache_userpermissions.push(doMakeRowProperty('Can Create', false, 'Builds'));
+
+                                cache_userpermissions.push(doMakeRowProperty('Can View', false, 'Templates'));
+                                cache_userpermissions.push(doMakeRowProperty('Can Create', false, 'Templates'));
+
+                                cache_userpermissions.push(doMakeRowProperty('Can View', false, 'Banking'));
+                                cache_userpermissions.push(doMakeRowProperty('Can Create', false, 'Banking'));
+
+                                cache_userpermissions.push(doMakeRowProperty('Can View', false, 'Purchasing'));
+                                cache_userpermissions.push(doMakeRowProperty('Can Create', false, 'Purchasing'));
+
+                                cache_userpermissions.push(doMakeRowProperty('Can View', false, 'Alerts'));
+                                cache_userpermissions.push(doMakeRowProperty('Can Create', false, 'Alerts'));
+
+                                cache_userpermissions.push(doMakeRowProperty('Can View', false, 'Dashboard'));
+                                cache_userpermissions.push(doMakeRowProperty('Can Create', false, 'Dashboard'));
+
+                                success({ total: cache_userpermissions.length, rows: cache_userpermissions });
+                            },
+                            columns:
+                                [
+                                    [
+                                        { field: 'name', title: 'Action', width: 100 },
+                                        {
+                                            field: 'value',
+                                            title: 'Permission',
+                                            width: 80,
+                                            formatter: function (value, row, index) {
+                                                return mapBoolToImage(value);
+                                            }
+                                        }
+                                    ]
+                                ]
+                        }
+                        );
+                },
+                buttons:
+                    [
+                        {
+                            text: 'Close',
+                            handler: function () {
+                                $('#dlgUserPermissions').dialog('close');
+                            }
+                        }
+                    ]
             }
-            );
+            ).dialog('center').dialog('open');
+
+
+        // *****************************
+        // doTreeGridGetSelectedRowData
+        //     (
+        //     'divPermissionTemplatesTG',
+        //     function (row) 
+        //     {
+        //         doServerDataMessage('newproducttemplate', { parentid: row.id, name: 'New Template' }, { type: 'refresh' });
+        //     }
+        //     );
     }
 
     function doClear() 
@@ -251,25 +420,26 @@ function doPermissionTemplatesTabWidgets()
 
                 doFooter();
             },
-            frozenColumns:
-                [
-                    [
-                        { title: 'Code', field: 'code', width: 200, align: 'left', resizable: true, editor: 'text', sortable: true }
-                    ]
-                ],
+            // frozenColumns:
+            //     [
+            //         [
+            //             // { title: 'Code', field: 'code', width: 200, align: 'left', resizable: true, editor: 'text', sortable: true },
+            //                 { title: 'Name', field: 'name', width: 500, align: 'left', resizable: true, editor: 'text', sortable: true }
+            //         ]
+            //     ],
             columns:
                 [
                     [
-                        { title: 'Name', field: 'name', width: 300, align: 'left', resizable: true, editor: 'text', sortable: true },
-                        { title: 'Client', field: 'clientid', width: 200, align: 'left', resizable: true, editor: { type: 'combobox', options: { valueField: 'id', textField: 'name', data: cache_clients, onSelect: function (record) {/*console.log(record);*/ } } }, formatter: function (value, row) { return doGetStringFromIdInObjArray(cache_clients, value); } },
-                        { title: 'Tax Code', field: 'taxcodeid', width: 200, align: 'left', resizable: true, editor: { type: 'combobox', options: { valueField: 'id', textField: 'name', data: cache_taxcodes, onSelect: function (record) {/*console.log(record);*/ } } }, formatter: function (value, row) { return doGetStringFromIdInObjArray(cache_taxcodes, value); } },
-                        { title: 'RRP', field: 'price', width: 150, align: 'right', resizable: true, editor: { type: 'numberbox', options: { groupSeparator: ',', precision: 2 } }, formatter: function (value, row, index) { return _.niceformatnumber(value); } },
-                        { title: 'Qty', field: 'qty', width: 150, align: 'right', resizable: true, editor: { type: 'numberbox', options: { groupSeparator: ',', precision: 0 } }, formatter: function (value, row, index) { return _.niceformatqty(value); } },
-                        { title: 'Total Cost', field: 'totalprice', width: 150, align: 'right', resizable: true, formatter: function (value, row, index) { return _.niceformatnumber(value); } },
-                        { title: '#Products', field: 'numproducts', width: 150, align: 'right', resizable: true },
-                        { title: 'Unit Cost', field: 'unitcost', width: 150, align: 'right', resizable: true, formatter: function (value, row, index) { return doCalcUnitCost(value, row, index); } },
-                        { title: 'Modified', field: 'date', width: 150, align: 'right', resizable: true, sortable: true },
-                        { title: 'By', field: 'by', width: 200, align: 'left', resizable: true, sortable: true }
+                        { title: 'Name', field: 'name', width: 500, align: 'left', resizable: true, editor: 'text', sortable: true },
+                        // { title: 'Client', field: 'clientid', width: 200, align: 'left', resizable: true, editor: { type: 'combobox', options: { valueField: 'id', textField: 'name', data: cache_clients, onSelect: function (record) {/*console.log(record);*/ } } }, formatter: function (value, row) { return doGetStringFromIdInObjArray(cache_clients, value); } },
+                        // { title: 'Tax Code', field: 'taxcodeid', width: 200, align: 'left', resizable: true, editor: { type: 'combobox', options: { valueField: 'id', textField: 'name', data: cache_taxcodes, onSelect: function (record) {/*console.log(record);*/ } } }, formatter: function (value, row) { return doGetStringFromIdInObjArray(cache_taxcodes, value); } },
+                        // { title: 'RRP', field: 'price', width: 150, align: 'right', resizable: true, editor: { type: 'numberbox', options: { groupSeparator: ',', precision: 2 } }, formatter: function (value, row, index) { return _.niceformatnumber(value); } },
+                        // { title: 'Qty', field: 'qty', width: 150, align: 'right', resizable: true, editor: { type: 'numberbox', options: { groupSeparator: ',', precision: 0 } }, formatter: function (value, row, index) { return _.niceformatqty(value); } },
+                        // { title: 'Total Cost', field: 'totalprice', width: 150, align: 'right', resizable: true, formatter: function (value, row, index) { return _.niceformatnumber(value); } },
+                        // { title: '#Products', field: 'numproducts', width: 150, align: 'right', resizable: true },
+                        // { title: 'Unit Cost', field: 'unitcost', width: 150, align: 'right', resizable: true, formatter: function (value, row, index) { return doCalcUnitCost(value, row, index); } },
+                        // { title: 'Modified', field: 'date', width: 150, align: 'right', resizable: true, sortable: true },
+                        // { title: 'By', field: 'by', width: 200, align: 'left', resizable: true, sortable: true }
                     ]
                 ],
             onContextMenu: function (e, row) 
